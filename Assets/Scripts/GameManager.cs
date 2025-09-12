e5cbf13891cde4ee1ea869a15d468549ea0aa618
Mari Benescu <mari.benescu.amm@gmail.com>
2025-09-12 12:33:25 +0200

    Buttons and menu prefabs
    I created buttons with predefined roles, assigned them the correspondent script to function and created the pause and setting panel as prefabs so that it can be used multiple times.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,9 +3,41 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject settingsPanel;
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Settings()
+    {
+        pauseMenu.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void backToPauseMenu()
+    {
+        pauseMenu.SetActive(true);
+        settingsPanel.SetActive(false);
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
     public void ChangeScene(string scene)
     {
         SceneManager.LoadScene(scene);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
     }
 
     public void Quit()
@@ -13,8 +45,5 @@ public class GameManager : MonoBehaviour
         Application.Quit();
     }
 
-    public void smth()
-    {
-
-    }
+   
 }
