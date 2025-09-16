a51062ffc18b089d4f33c1b23249bea8992ebb91
Mr. Pancake - René <renehammink@icloud.com>
2025-09-16 12:42:14 +0200

    grinding system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 
 public class PlayerController : MonoBehaviour
@@ -9,11 +10,18 @@ public class PlayerController : MonoBehaviour
     
     //References
     private Rigidbody rb;
+    private BoxCollider grindHitbox;
     private InputManager inputManager;
     
     [Header("Player Settings")]
     [SerializeField] private float playerSpeed;
     [SerializeField] private float jumpHight;
+    [SerializeField] private float coyoteTime; //not implemented yet
+
+    [Header("GrindHitbox")]
+    [SerializeField] private Vector3 halfExtends;
+    [SerializeField] private Vector3 center;
+    [SerializeField] private LayerMask grindLayerMask;
 
     private void Awake()
     {
@@ -27,6 +35,7 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
+        grindHitbox = gameObject.GetComponent<BoxCollider>();
         inputManager = InputManager.Instance;
     }
 
@@ -38,49 +47,112 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         bool grounded = GroundedCheck();
+        bool OnRail = OnGrindCheck();
+        
+        Debug.Log($"Grounded: {grounded}, OnRail: {OnRail}");
         rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y, playerSpeed);
-        if (inputManager.jump && grounded)
+        if (inputManager.jump && (grounded || OnRail))
         {
             rb.AddForce(transform.up * jumpHight, ForceMode.Impulse);
-            switch (inputManager.trick)
+            DoTrick();
+        }
+
+        if (OnRail && inputManager.trick != TrickDirection.None && rb.linearVelocity.y < 0)
+        {
+            grindHitbox.enabled = true;
+            DoGrind();
+        }
+        
+        ResetVariables(grounded);
+    }
+
+    private void DoTrick()
+    {
+        switch (inputManager.trick)
+        {
+            case TrickDirection.Up:
             {
-                case TrickDirection.Up:
-                {
-                    Debug.Log("UpTrick");
-                    break;
-                }
-                case TrickDirection.Down:
-                {
-                    Debug.Log("DownTrick");
-                    break;
-                }
-                case TrickDirection.Left:
-                {
-                    Debug.Log("LeftTrick");
-                    break;
-                }
-                case TrickDirection.Right:
-                {
-                    Debug.Log("RightTrick");
-                    break;
-                }
-                case TrickDirection.None:
-                {
-                    Debug.Log("Ollie");
-                    break;
-                }
+                Debug.Log("UpTrick");
+                break;
+            }
+            case TrickDirection.Down:
+            {
+                Debug.Log("DownTrick");
+                break;
+            }
+            case TrickDirection.Left:
+            {
+                Debug.Log("LeftTrick");
+                break;
+            }
+            case TrickDirection.Right:
+            {
+                Debug.Log("RightTrick");
+                break;
+            }
+            case TrickDirection.None:
+            {
+                Debug.Log("Ollie");
+                break;
             }
         }
-        inputManager.ResetInputs();
+    }
+    private void DoGrind()
+    {
+        switch (inputManager.trick)
+        {
+            case TrickDirection.Up:
+            {
+                Debug.Log("UpGrind");
+                break;
+            }
+            case TrickDirection.Down:
+            {
+                Debug.Log("DownGrind");
+                break;
+            }
+            case TrickDirection.Left:
+            {
+                Debug.Log("LeftGrind");
+                break;
+            }
+            case TrickDirection.Right:
+            {
+                Debug.Log("RightGrind");
+                break;
+            }
+            case TrickDirection.None:
+            {
+                Debug.Log("No Grind");
+                break;
+            }
+        }
     }
 
     private bool GroundedCheck()
     {
-        LayerMask layerMask = LayerMask.GetMask("Ground");
-        if (Physics.Raycast(transform.position, -transform.up, transform.localScale.y + 0.1f, layerMask))
+        LayerMask groundLayerMask = LayerMask.GetMask("Ground");
+        return Physics.Raycast(transform.position, -transform.up, transform.localScale.y + 0.1f, groundLayerMask);
+    }
+
+    private bool OnGrindCheck()
+    {
+        return Physics.CheckBox(transform.position + center, halfExtends, Quaternion.identity, grindLayerMask);
+    }
+
+    private void ResetVariables(bool grounded)
+    {
+        inputManager.ResetInputs();
+
+        if (inputManager.trick == TrickDirection.None || grounded)
         {
-            return true;
+            grindHitbox.enabled = false;
         }
-        return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position + center, 2 * halfExtends);
     }
 }
